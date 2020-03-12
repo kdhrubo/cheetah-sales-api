0cf5e8e53d098cd6505571d840336c69550678aa
Dhrubo Kayal <dhrubo.kayal@gmail.com>
2020-03-11 22:59:15 -0500

    fix lead creation

--- cheetah-sales-api/src/main/java/com/cheetahapps/sales/controller/AccountController.java
+++ cheetah-sales-api/src/main/java/com/cheetahapps/sales/controller/AccountController.java
@@ -5,15 +5,13 @@ package com.cheetahapps.sales.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.cheetahapps.sales.business.AccountBusinessDelegate;
 import com.cheetahapps.sales.domain.Account;
 
-@Controller
+@RestController
 @RequestMapping("/accounts")
-
 public class AccountController extends AbstractBaseController<Account, String> {
 	
 	private final AccountBusinessDelegate accountBusinessDelegate;
--- cheetah-sales-api/src/main/java/com/cheetahapps/sales/controller/OpportunityController.java
+++ cheetah-sales-api/src/main/java/com/cheetahapps/sales/controller/OpportunityController.java
@@ -1,13 +1,12 @@
 package com.cheetahapps.sales.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.cheetahapps.sales.business.OpportunityBusinessDelegate;
 import com.cheetahapps.sales.domain.Opportunity;
 
-@Controller
+@RestController
 @RequestMapping("/opportunities")
 
 public class OpportunityController extends AbstractBaseController<Opportunity, String> {
