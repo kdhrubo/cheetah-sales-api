d527abd2dcaab5c4ac6721f7ad2a44d8b8a382a4
Dhrubo <dhrubo.kayal@gmail.com>
2020-04-11 23:25:21 -0500

    cleanup and form changes from picklist to select

--- cheetah-sales-api/src/main/java/com/cheetahapps/sales/contact/Contact.java
+++ cheetah-sales-api/src/main/java/com/cheetahapps/sales/contact/Contact.java
@@ -27,91 +27,67 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Contact extends ExtensibleBase{
-	
+public class Contact extends ExtensibleBase {
+
 	@DBRef
 	private PickList salutation;
 
 	private String firstName;
-	
 
 	private String lastName;
-	
+
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dob;
-	
-	
 
 	private String email;
-	
+
 	private String mobile;
-	
+
 	private String officePhone;
-	
-	
-	
+
 	private String otherEmail;
-	
+
 	private String otherPhone;
-	
+
 	private String fax;
-	
-	
-
-
 
 	private Account account;
+
 	
-	
-	
-	@DBRef
-	private PickList title;
-	
+	private String designation;
 
 	private String department;
-	
 
 	private Contact reportsTo;
 
 	private PickList leadSource;
 
-	
-	
-	
 	private User assignedUser;
 
 	private Team assignedTeam;
-	
-	
+
 	private PickList contactType;
 
 	private PickList contactStatus;
-	
-	
 
 	private boolean donotCall;
 
 	private boolean emailOptIn;
-	
+
 	private boolean smsOptIn;
 
 	private boolean notifyOwner;
 
-	
-
 	private String twitter;
 
 	private String facebook;
-	
+
 	private String linkedIn;
-	
 
 	private Address primaryAddress;
 
 	private Address secondaryAddress;
 
 	private String description;
-	
-	
-	
+
 }
