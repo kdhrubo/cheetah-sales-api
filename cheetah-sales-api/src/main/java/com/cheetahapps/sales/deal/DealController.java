51021402745d71286ee0f7a5a205898c2d7b8c43
Rajiv Kumar <rajiv.kumar.102@gmail.com>
2020-05-01 21:49:43 +0530

    #Deal Search controller

--- cheetah-sales-api/src/main/java/com/cheetahapps/sales/deal/DealController.java
+++ cheetah-sales-api/src/main/java/com/cheetahapps/sales/deal/DealController.java
@@ -1,18 +1,36 @@
 package com.cheetahapps.sales.deal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import com.cheetahapps.sales.core.AbstractBaseController;
+import com.cheetahapps.sales.lead.Lead;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/deals")
 
 public class DealController extends AbstractBaseController<Deal, String> {
 
+	DealBusinessDelegate dealBusinessDelegate;
+	
 	@Autowired
-	public DealController(DealBusinessDelegate service) {
-		super(service);
+	public DealController(DealBusinessDelegate dealBusinessDelegate) {
+		super(dealBusinessDelegate);
+		this.dealBusinessDelegate = dealBusinessDelegate;
+	}
+	
+	@GetMapping("/search")
+	public Page<Deal> search(@RequestParam("rsql") String rsql, @PageableDefault Pageable pageable) {
+		log.info("Deal business delegate - {}", this.businessDelegate);
+		log.info("rsql - {}", rsql);
+		log.info("pageable - {}", pageable);
+		return dealBusinessDelegate.search(rsql, pageable);
 	}
 
 }
