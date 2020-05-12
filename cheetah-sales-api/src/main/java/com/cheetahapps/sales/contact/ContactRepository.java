f41ed29965443719e74263a6ce2ca2c107b4eed7
Dhrubo <dhrubo.kayal@gmail.com>
2020-05-12 16:59:23 -0500

    fixes

--- cheetah-sales-api/src/main/java/com/cheetahapps/sales/contact/ContactRepository.java
+++ cheetah-sales-api/src/main/java/com/cheetahapps/sales/contact/ContactRepository.java
@@ -2,12 +2,9 @@ package com.cheetahapps.sales.contact;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.cheetahapps.sales.core.SearchRepositoryCustom;
 
-
-@Repository
-public interface ContactRepository extends MongoRepository<Contact, String>, SearchRepositoryCustom<Contact> {
+interface ContactRepository extends MongoRepository<Contact, String>, SearchRepositoryCustom<Contact> {
 	
 }
