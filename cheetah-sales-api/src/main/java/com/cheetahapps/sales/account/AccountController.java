5d6fb8c3a343f88dd3c9a7612943cb448269e56d
Rajiv Kumar <rajiv.kumar.102@gmail.com>
2020-05-17 21:51:43 +0530

    #Import account csv

--- cheetah-sales-api/src/main/java/com/cheetahapps/sales/account/AccountController.java
+++ cheetah-sales-api/src/main/java/com/cheetahapps/sales/account/AccountController.java
@@ -2,13 +2,21 @@ package com.cheetahapps.sales.account;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import com.cheetahapps.sales.core.AbstractBaseController;
+import com.cheetahapps.sales.lead.Lead;
+import com.cheetahapps.sales.lead.LeadController;
+import com.cheetahapps.sales.lead.LeadList;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/accounts")
 class AccountController extends AbstractBaseController<Account, String> {
@@ -24,5 +32,11 @@ class AccountController extends AbstractBaseController<Account, String> {
 	@GetMapping("/q")
 	public Page<Account> search(@RequestParam("rsql") String rsql, Pageable pageable) {
 		return accountBusinessDelegate.search(rsql, pageable);
+	}
+	
+	@PostMapping("/import")
+	public List<Account> saveAll(@RequestBody AccountList accountWrapper) {
+		log.info("## Saving all leads by import api  --> {}", accountWrapper);
+		return accountBusinessDelegate.saveAll(accountWrapper.getAccounts());
 	}
 }
